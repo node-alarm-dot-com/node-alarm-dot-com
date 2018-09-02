4061104e34713bcc497ae174e341c6f855184208
Mike Kormendy <info@mikekormendy.com>
2018-09-02 00:37:12 -0500

    Adds getstate example.

---
+++ examples/getstate.js
@@ -0,0 +1,43 @@
+const frontpoint = require('..')
+
+main()
+
+function main() {
+  const yargs = require('yargs')
+    .usage('Usage: $0 [options]')
+    .option('u', {
+      alias: 'username',
+      default: process.env.USERNAME,
+      defaultDescription: 'USERNAME environment variable',
+      describe: 'Username',
+      type: 'string'
+    })
+    .option('p', {
+      alias: 'password',
+      default: process.env.PASSWORD,
+      defaultDescription: 'PASSWORD environment variable',
+      describe: 'Password',
+      type: 'string'
+    })
+    .help('h')
+    .alias('h', 'help')
+    .version(false)
+
+  const username = yargs.argv.username
+  const password = yargs.argv.password
+  if (!username || !password) {
+    console.error(`Error: username and password are required`)
+    yargs.showHelp()
+    return process.exit(1)
+  }
+
+  frontpoint
+    .login(username, password)
+    .then(res => frontpoint.getCurrentState(res.systems[0], res))
+    .then(res => {
+      console.log(Object.keys(frontpoint.SYSTEM_STATES)[parseInt(res.partitions[0].attributes.state)])
+    })
+    .catch(err => {
+      console.error(err)
+    })
+}
