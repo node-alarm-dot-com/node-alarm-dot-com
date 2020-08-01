9da2083fcfd916f64fd89ef1188aa9edc080937e
Mike Kormendy <info@mikekormendy.com>
2020-07-31 23:10:34 -0500

    Fixes error with systems that don't have garage doors producing undefined error.

--- index.js
+++ index.js
@@ -236,8 +236,7 @@ function getCurrentState(systemID, authOpts) {
           sensors: typeof sensors != 'undefined' ? sensors.data : [],
           lights: typeof lights != 'undefined' ? lights.data : [],
           locks: typeof locks != 'undefined' ? locks.data : [],
-          garages: garageDoors.data,
-          //garages: typeof garages != 'undefined' ? garageDoors.data : [],
+          garages: typeof locks != 'undefined' ? garageDoors.data : [],
           relationships: rels
         }
       })
