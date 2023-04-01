8c1611c065a7d31246839d86882bdd5064f3843a
Chase Lau <chase9@mac.com>
2023-04-01 10:54:26 -0500

    Update IdentityResponse.ts

--- src/_models/IdentityResponse.ts
+++ src/_models/IdentityResponse.ts
@@ -4,99 +4,43 @@
  * It's then parsed and the authentication token stored for future use.
  */
 export interface IdentityResponse {
-  data: {
-    id: number;
-    type: 'identity';
-    attributes: {
-      timezone: string;
-      initialSetupSteps: [];
-      helpTours: number[];
-      logoSource: string;
-      logoName: string;
-      dealerPath: string;
-      favIconUrl: string;
-      poweredByPath: string;
-      copyrightInfo: string;
-      trademarkInfo: string;
-      hasCompletedOnboarding: boolean;
-      isEnterprise: boolean;
-      isAccessControl: boolean;
-      isCommercial: boolean;
-      isPointCentral: boolean;
-      isPcRpm: boolean;
-      isPointCentralSTR: boolean;
-      isMobileApp: boolean;
-      useWebsockets: boolean;
-      websocketServiceEndpoint: string;
-      enabledWebsocketMessageHandlers: null;
-      canBrowserEnableFlashDirectly: boolean;
-      supportsEnableFlashPrompt: boolean;
-      supportedNativeBridgeFeatures: null;
-      hasTroubleConditionsService: boolean;
-      errorReportingConfiguration: {
-        providerName: string;
-        apiKey: null;
-        scriptUrl: null;
-        isEnabled: boolean;
-        machineName: string;
-        logErrorsToConsole: boolean;
-        showNotificationOnError: boolean;
-        redirectToErrorRouteOnUnhandledError: boolean;
-        environment: string;
-        servicePlan: string;
-      };
-      enableHomeScreenConfiguration: boolean;
-      accountType: number;
-      externalTerms: string;
-      externalPrivacyPolicy: string;
-      applicationSessionProperties: {
-        shouldTimeout: boolean;
-        keepAliveUrl: string;
-        enableKeepAlive: boolean;
-        logoutTimeoutMs: number;
-        inactivityWarningTimeoutMs: number;
-      };
-      postLoginRedirect: null;
-      canDonateClips: boolean;
-      hasInternalBetaAccess: boolean;
+  data: IdentityData[];
+  relationships: {
+    dealer: {
+      data: Relationship;
     };
-    relationships: {
-      dealer: {
-        data: Relationship;
+    primaryColor: {
+      data: Relationship;
+    };
+    secondaryColor: {
+      data: Relationship;
+    };
+    navColor: {
+      data: Relationship;
+    };
+    profile: {
+      data: Relationship;
+    };
+    accountInformation: {
+      data: Relationship;
+    };
+    availableRouteItems: {
+      data: Relationship[];
+      meta: {
+        count: string;
       };
-      primaryColor: {
-        data: Relationship;
-      };
-      secondaryColor: {
-        data: Relationship;
-      };
-      navColor: {
-        data: Relationship;
-      };
-      profile: {
-        data: Relationship;
-      };
-      accountInformation: {
-        data: Relationship;
-      };
-      availableRouteItems: {
-        data: Relationship[];
-        meta: {
-          count: string;
-        };
-      };
-      wizard: {
-        data: Relationship[] | null;
-      };
-      clipDonationAgreement: {
-        data: Relationship;
-      };
-      selectedSystem: {
-        data: Relationship;
-      };
-      selectedSystemGroup: {
-        data: Relationship | null;
-      };
+    };
+    wizard: {
+      data: Relationship[] | null;
+    };
+    clipDonationAgreement: {
+      data: Relationship;
+    };
+    selectedSystem: {
+      data: Relationship;
+    };
+    selectedSystemGroup: {
+      data: Relationship | null;
     };
   };
   included: any[];
@@ -105,23 +49,98 @@ export interface IdentityResponse {
   };
 }
 
+export interface IdentityData {
+  id: number;
+  type: 'identity';
+  attributes: {
+    timezone: string;
+    preferredTimezone: string;
+    initialSetupSteps: [];
+    helpTours: number[];
+    logoSource: string;
+    logoName: string;
+    dealerPath: string;
+    mobileAppBrand: string;
+    favIconUrl: string;
+    poweredByPath: string;
+    copyrightInfo: string;
+    trademarkInfo: string;
+    hasCompletedOnboarding: boolean;
+    isEnterprise: boolean;
+    isAccessControl: boolean;
+    isCommercial: boolean;
+    isPointCentral: boolean;
+    isPcRpm: boolean;
+    isPointCentralSTR: boolean;
+    isPointCentralAccessControl: boolean;
+    isPointCentralAccessControlSTR: boolean;
+    isUsingEnterpriseLocationsCache: boolean;
+    isMobileApp: boolean;
+    isMobileTechApp: boolean;
+    isVisualIdentification: boolean;
+    isEscalatedEvents: boolean;
+    isWindowsDevice: boolean;
+    applicationBuildNumber: number;
+    canViewHighlights: boolean;
+    canViewVideoClips: boolean;
+    hasAccessToAudioForNonDoorbellCameras: boolean;
+    hasSmartArmingSensorBased: boolean;
+    isSmartDisarmEligible: boolean;
+    canEditNoiseSensors: boolean;
+    isManagedAccessAccount: boolean;
+    supportedNativeBridgeFeatures: null;
+    supportedNativeViewTransitions: null;
+    supportedDisplayModeFeatures: null;
+    hasTroubleConditionsService: boolean;
+    errorReportingConfiguration: {
+      providerName: string;
+      apiKey: null;
+      scriptUrl: null;
+      isEnabled: boolean;
+      machineName: string;
+      logErrorsToConsole: boolean;
+      showNotificationOnError: boolean;
+      redirectToErrorRouteOnUnhandledError: boolean;
+      environment: string;
+      servicePlan: string;
+    };
+    enableHomeScreenConfiguration: boolean;
+    accountType: number;
+    externalTerms: string;
+    externalPrivacyPolicy: string;
+    shouldHidePrivacyPolicyLink: boolean;
+    shouldHideTermsAndConditionsLink: boolean;
+    applicationSessionProperties: {
+      shouldTimeout: boolean;
+      keepAliveUrl: string;
+      enableKeepAlive: boolean;
+      logoutTimeoutMs: number;
+      inactivityWarningTimeoutMs: number;
+    };
+    postLoginRedirect: null;
+    localizeTempUnitsToCelsius: boolean;
+    canDonateClips: boolean;
+    hasInternalBetaAccess: boolean;
+  }
+}
+
 export interface Relationship {
   id: string;
   type:
-    | 'devices/partition'
-    | 'devices/lock'
-    | 'video/camera'
-    | 'devices/garage-door'
-    | 'automation/scene'
-    | 'devices/sensor'
-    | 'devices/light'
-    | 'devices/thermostat'
-    | 'geolocation/geo-device'
-    | 'geolocation/fence'
-    | 'systems/configuration'
-    | 'navigation/route-item'
-    | 'dealers/dealer'
-    | 'ui/color'
-    | 'profile/profile'
-    | 'accountInformation/account-information';
+  | 'devices/partition'
+  | 'devices/lock'
+  | 'video/camera'
+  | 'devices/garage-door'
+  | 'automation/scene'
+  | 'devices/sensor'
+  | 'devices/light'
+  | 'devices/thermostat'
+  | 'geolocation/geo-device'
+  | 'geolocation/fence'
+  | 'systems/configuration'
+  | 'navigation/route-item'
+  | 'dealers/dealer'
+  | 'ui/color'
+  | 'profile/profile'
+  | 'accountInformation/account-information';
 }
