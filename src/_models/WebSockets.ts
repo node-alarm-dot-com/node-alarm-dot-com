0ccc169d3ae369805c68cbad87b019c2c7866848
Chase Christiansen <chase9@mac.com>
2026-05-28 09:38:28 -0500

    Added WS event IDs
    Thank you to https://github.com/pyalarmdotcom/pyalarmdotcomajax for mapping these!

--- src/_models/WebSockets.ts
+++ src/_models/WebSockets.ts
@@ -22,3 +22,148 @@ export interface WebSocketEvent {
   QstringForExtraData: string;
   DeviceType: number;
 }
+
+export enum WebSocketEventTypes {
+  // Partition / Security System
+  Disarmed = 8,
+  ArmedStay = 9,
+  ArmedAway = 10,
+  ArmedNight = 113,
+  Alarm = 1,
+  AlarmCancelled = 238,
+  PendingAlarm = 62,
+  ArmingSupervisionFault = 48,
+  DisarmingSupervisionFault = 47,
+
+  // Sensors
+  Closed = 0,
+  Opened = 15,
+  Bypassed = 13,
+  EndOfBypass = 35,
+  Tamper = 7,
+  DoorLeftOpen = 101,
+  DoorLeftOpenRestoral = 103,
+  OpenedClosed = 100,
+
+  // Locks
+  DoorUnlocked = 90,
+  DoorLocked = 91,
+  DoorAccessed = 92,
+  BadLockUserCode = 93,
+  DoorAccessedDoubleSwipe = 236,
+  DoorBuzzedFromWebsite = 182,
+  DoorFailedAccess = 180,
+  DoorForcedOpen = 181,
+  DoorHeldOpen = 184,
+  WrongPinCode = 398,
+
+  // Lights & Switches
+  LightTurnedOn = 315,
+  LightTurnedOff = 316,
+  SwitchLevelChanged = 317,
+
+  // Thermostats
+  ThermostatSetPointChanged = 94,
+  ThermostatModeChanged = 95,
+  ThermostatOffset = 105,
+  ThermostatFanModeChanged = 120,
+
+  // Image Sensors
+  ImageSensorUpload = 99,
+
+  // Panic
+  AuxiliaryPanic = 17,
+  AuxPanicPendingAlarm = 61,
+  AuxPanicSuspectedAlarm = 65,
+  ExitButtonPressed = 141,
+  FirePanic = 24,
+  InAppAuxiliaryPanic = 201,
+  InAppFirePanic = 200,
+  InAppPolicePanic = 202,
+  InAppSilentPolicePanic = 203,
+  PolicePanic = 22,
+  PolicePanicSuspectedAlarm = 64,
+  SilentPolicePanic = 73,
+  SilentPolicePanicSuspectedAlarm = 172,
+
+  // Commercial Schedules
+  CommercialClosedOnTime = 127,
+  CommercialClosedUnexpectedly = 177,
+  CommercialEarlyClose = 125,
+  CommercialEarlyOpen = 122,
+  CommercialLateClose = 126,
+  CommercialLateOpen = 123,
+  CommercialOpenOnTime = 124,
+
+  // Sump Pump
+  SumpPumpAlertCriticalIssueMalfunction = 118,
+  SumpPumpAlertCriticalIssueOff = 117,
+  SumpPumpAlertIdle = 114,
+  SumpPumpAlertNormalOperation = 115,
+  SumpPumpAlertPossibleIssue = 116,
+
+  // Network / Router
+  NetworkDhcpReservationsUpdated = 433,
+  NetworkDhcpSettingsUpdated = 432,
+  NetworkMapUpdated = 391,
+  NetworkPortForwardingUpdated = 434,
+  RouterHostsUpdated = 450,
+  RouterProfilesUpdated = 451,
+  SpeedTestResultsUpdated = 454,
+
+  // Video
+  GoogleSdmEvent = 346,
+  VideoAnalyticsDetection = 210,
+  VideoAnalytics2Detection = 302,
+  VideoCameraTriggered = 71,
+  VideoEventTriggered = 76,
+
+  // Package Detection
+  PackageDeliveryAlert = 363,
+  PackageRetrievalAlert = 364,
+
+  // Access Control
+  AccessControlDoorAccessGranted = 298,
+  UnknownCardFormatRead = 185,
+
+  // Misc
+  UserLoggedIn = 55,
+  ViewedByCentralStation = 158
+}
+
+export const LOCK_EVENT_TYPES = new Set([WebSocketEventTypes.DoorLocked, WebSocketEventTypes.DoorUnlocked]);
+
+export const SENSOR_EVENT_TYPES = new Set([
+  WebSocketEventTypes.Opened,
+  WebSocketEventTypes.Closed,
+  WebSocketEventTypes.Tamper,
+  WebSocketEventTypes.Bypassed,
+  WebSocketEventTypes.EndOfBypass,
+  WebSocketEventTypes.DoorLeftOpen,
+  WebSocketEventTypes.DoorLeftOpenRestoral
+]);
+
+export const PARTITION_EVENT_TYPES = new Set([
+  WebSocketEventTypes.ArmedAway,
+  WebSocketEventTypes.ArmedStay,
+  WebSocketEventTypes.ArmedNight,
+  WebSocketEventTypes.Disarmed,
+  WebSocketEventTypes.Alarm,
+  WebSocketEventTypes.AlarmCancelled,
+  WebSocketEventTypes.PendingAlarm
+]);
+
+export const LIGHT_EVENT_TYPES = new Set([
+  WebSocketEventTypes.LightTurnedOn,
+  WebSocketEventTypes.LightTurnedOff,
+  WebSocketEventTypes.SwitchLevelChanged
+]);
+
+export const GARAGE_EVENT_TYPES = new Set([WebSocketEventTypes.Opened, WebSocketEventTypes.Closed]);
+
+export const THERMOSTAT_EVENT_TYPES = new Set([
+  WebSocketEventTypes.ThermostatSetPointChanged,
+  WebSocketEventTypes.ThermostatModeChanged,
+  WebSocketEventTypes.ThermostatFanModeChanged,
+  WebSocketEventTypes.ThermostatOffset
+]);
